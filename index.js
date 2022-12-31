a1db863abab7d3ed39cfe401fa49d4fe803e38f2
Amit <cliphero4@gmail.com>
2022-12-31 10:21:11 +0530

    Basic server for vps

---
+++ index.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/" + "index.html");
+});
+app.get("/hello", (req, res) => {
+  res.sendFile(__dirname + "/public/" + "hello.html");
+});
+app.get("/data", (req, res) => {
+  //res.header("Content-Type", "application/json");
+  res.sendFile(__dirname + "/public/" + "data.json");
+});
+app.post("/hello", (req, res) => {
+  //console.log(req.body.Name);
+  //res.send("hey" + req.body.Name);
+  var userData = req.body.Name;
+  fs.readFile(
+    path.resolve(__dirname, "./public/hello.html"),
+    "utf8",
+    function (err, data) {
+      if (err) throw err;
+      var $ = cheerio.load(data);
+      //console.log(data);
+      $(".greeting").text("Hello " + userData);
+      res.send($.html());
+    }
+  );
+});
+app.listen(port, () => {
+  console.log("App running on port 3000");
+});
